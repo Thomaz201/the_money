e4cdfac0b0c5f6161d135cd66399251313e9e267
Lucas Thomaz <lucasthomazr@hotmail.com>
2021-05-15 12:31:25 -0300

    Modal configured with react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+  onButtonClickFunction: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({ onButtonClickFunction }) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="the money" />
-        <button type="button">
+        <button type="button" onClick={onButtonClickFunction}>
           Nova transação
         </button>
       </Content>
